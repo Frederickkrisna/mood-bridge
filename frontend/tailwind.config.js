6b529cc5a1a461a33750aeea84915095d0e3d126
Admantix <williamwijaya499@gmail.com>
2024-12-02 11:44:27 +0700

    Initialize frontend project with Vite, React, and TypeScript setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const preset = require("./themes/presets");
+const tokens = require("./themes/tokens");
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      ...preset,
+      ...tokens,
+    },
+  },
+  plugins: [],
+};
