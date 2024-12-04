31290840f07d9ffae19debd536f8e4f737f6cfd5
Admantix <williamwijaya499@gmail.com>
2024-12-05 01:28:28 +0700

    Update app icon and enhance Home component with new images; refactor AppSidebar for improved layout and theme handling

--- front-end/src/app/dashboard/home.tsx
+++ front-end/src/app/dashboard/home.tsx
@@ -2,50 +2,101 @@ import { AuthContext } from "@/context/AuthContext";
 import Layout from "../layout";
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
 import {
-  IconArrowWaveRightUp,
-  IconClipboardCopy,
-  IconFileBroken,
-  IconSignature,
-  IconTableColumn,
+  Icon24Hours,
+  IconActivityHeartbeat,
+  IconChartAreaLine,
+  IconMoodCheck,
+  IconWriting,
 } from "@tabler/icons-react";
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import SentimentAnalysis from "@/assets/sentiment_analysis.png";
+import MoodTracker from "@/assets/mood_tracker.jpg";
+import Journaling from "@/assets/journaling.gif";
+import AnonymousChat from "@/assets/texting.jpg";
+import CallCenter from "@/assets/call_center.jpg";
 
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+const Image1 = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
+    <img
+      src={SentimentAnalysis}
+      alt="Sentiment Analysis"
+      className="object-cover w-full h-full"
+    />
+  </div>
+);
+
+const Image2 = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
+    <img
+      src={MoodTracker}
+      alt="Mood Tracker"
+      className="object-cover w-full h-full"
+    />
+  </div>
+);
+
+const Image3 = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
+    <img
+      src={Journaling}
+      alt="Journaling"
+      className="object-cover w-full h-full"
+    />
+  </div>
+);
+
+const Image4 = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
+    <img
+      src={AnonymousChat}
+      alt="Anonymous Chat"
+      className="object-cover w-full h-full"
+    />
+  </div>
+);
+
+const Image5 = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
+    <img
+      src={CallCenter}
+      alt="Call Center"
+      className="object-cover w-full h-full"
+    />
+  </div>
 );
 
 const items = [
   {
     title: "Sentiment Analysis",
     description: "Analyze your feelings and emotions with AI technology.",
-    header: <Skeleton />,
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+    header: <Image1 />,
+    icon: <IconMoodCheck className="h-4 w-4 text-neutral-500" />,
   },
   {
     title: "Mood Tracker",
     description: "Take a look back how's your mood progressions.",
-    header: <Skeleton />,
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    header: <Image2 />,
+    icon: <IconChartAreaLine className="h-4 w-4 text-neutral-500" />,
   },
   {
     title: "Journaling and Reflections",
     description: "Write down your thoughts and reflections.",
-    header: <Skeleton />,
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    header: <Image3 />,
+    icon: <IconWriting className="h-4 w-4 text-neutral-500" />,
   },
   {
     title: "Chat with fellow sufferers",
     description:
       "Understand that you are not alone in this journey. Chat with others.",
-    header: <Skeleton />,
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    header: <Image4 />,
+    icon: <IconActivityHeartbeat className="h-4 w-4 text-neutral-500" />,
   },
   {
     title: "24/7 Support",
     description: "Get help from our team of experts.",
-    header: <Skeleton />,
-    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+    header: <Image5 />,
+    icon: <Icon24Hours className="h-4 w-4 text-neutral-500" />,
   },
 ];
 
@@ -69,7 +120,7 @@ export default function Home() {
               description={item.description}
               header={item.header}
               icon={item.icon}
-              className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+              className={`${i === 3 || i === 6 ? "md:col-span-2" : ""} `}
               onClick={() => console.log("clicked")}
             />
           ))}
--- front-end/src/components/app-sidebar.tsx
+++ front-end/src/components/app-sidebar.tsx
@@ -27,6 +27,7 @@ import {
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ModeToggle } from "./mode-toggle";
+import { useTheme } from "./theme-provider";
 
 const items = [
   {
@@ -52,6 +53,7 @@ const items = [
 ];
 
 export function AppSidebar() {
+  const { theme } = useTheme();
   const navigate = useNavigate();
   const { userData } = useContext(AuthContext);
   const handleSignOut = () => {
@@ -72,15 +74,15 @@ export function AppSidebar() {
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet"
       ></link>
-      <Sidebar>
+      <Sidebar collapsible="none" className="flex flex-col h-screen border-r-2">
         <SidebarHeader />
         <SidebarGroupLabel>
-          <div className="text-3xl mx-3">Mood Bridge</div>
+          <div className="text-3xl">Mood Bridge</div>
           <div className="ml-auto">
             <ModeToggle />
           </div>
         </SidebarGroupLabel>
-        <div className="my-auto mx-3">
+        <div className="mt-5 mx-3 flex-grow h-auto">
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
@@ -100,37 +102,53 @@ export function AppSidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </div>
-        <SidebarFooter>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <SidebarMenuButton>
-                    <User2 /> {userData.first_name}, {userData.last_name}
-                    <ChevronUp className="ml-auto" />
-                  </SidebarMenuButton>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  side="top"
-                  className="w-[--radix-popper-anchor-width] space-y-1 mb-3"
-                >
-                  <DropdownMenuItem>
-                    <button
-                      onClick={() => {
-                        navigate("/dashboard/account");
-                      }}
-                    >
-                      Account
-                    </button>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <button onClick={handleSignOut}>Sign out</button>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarFooter>
+        <div className="mt-auto">
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <SidebarMenuButton className="font-semibold text-lg h-auto">
+                      <User2 /> {userData.first_name} {userData.last_name}
+                      <ChevronUp className="ml-auto" />
+                    </SidebarMenuButton>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent
+                    side="top"
+                    className="w-[--radix-popper-anchor-width] space-y-1 mb-3 rounded-lg"
+                  >
+                    <DropdownMenuItem>
+                      <button
+                        onClick={() => {
+                          navigate("/dashboard/account");
+                        }}
+                        className={`text-right ${
+                          theme === "light"
+                            ? "hover:bg-slate-100"
+                            : "hover:bg-slate-800"
+                        } min-w-full px-2 rounded-sm`}
+                      >
+                        Account
+                      </button>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <button
+                        onClick={handleSignOut}
+                        className={`text-right ${
+                          theme === "light"
+                            ? "hover:bg-slate-100 "
+                            : "hover:bg-slate-800 "
+                        } min-w-full px-2 rounded-sm text-red-600 font-bold`}
+                      >
+                        Log out
+                      </button>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </div>
       </Sidebar>
     </>
   );
