fe8aab184be8d6f8893f936a516eb4f04029c128
Admantix <williamwijaya499@gmail.com>
2024-12-06 16:46:19 +0700

    ForumCard Components Done

--- front-end/src/app/dashboard/forum.tsx
+++ front-end/src/app/dashboard/forum.tsx
@@ -3,11 +3,19 @@ import Layout from "../layout";
 import ForumCard from "@/components/forum-card";
 
 export default function Forum() {
+  const dummyItem = {
+    mood: "Positive",
+    userId: "Ni Hao",
+    createdAt: "20 Dec, 2024",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas",
+    postId: "1",
+  };
   return (
     <>
       <Layout>
-        <div className="scroll-container ml-[45vh] flex flex-col min-w-[50vw] max-w-[50vw] overflow-x-hidden">
-          <ForumCard />
+        <div className="scroll-container ml-[48vh] flex flex-col min-w-[50vw] max-w-[50vw] overflow-x-hidden">
+          <ForumCard {...dummyItem} />
         </div>
       </Layout>
       <RightSidebar />
--- front-end/src/components/forum-card.tsx
+++ front-end/src/components/forum-card.tsx
@@ -1,34 +1,42 @@
-import { ArrowDown, ChevronDown, Salad } from "lucide-react";
+import { ChevronDown, Salad } from "lucide-react";
 import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
+import React from "react";
+import { PostInterface } from "@/interfaces/interface";
+import Positive from "@/assets/Positive.png";
+import Negative from "@/assets/Negative.png";
+import Neutral from "@/assets/Neutral.png";
 
-export default function ForumCard() {
+const ForumCard: React.FC<PostInterface> = (props) => {
   const Emoji = () => {
-    return (
-      <div className="flex items-center justify-center">
-        <Salad />
-      </div>
-    );
+    switch (props.mood) {
+      case "Positive":
+        return (
+          <img src={Positive} alt="positive" className="w-8 h-8 object-cover" />
+        );
+      case "Negative":
+        return (
+          <img src={Negative} alt="negative" className="w-8 h-8 object-cover" />
+        );
+      case "Neutral":
+        return (
+          <img src={Neutral} alt="neutral" className="w-8 h-8 object-cover" />
+        );
+      default:
+        return <Salad />;
+    }
   };
 
   return (
     <Card className="border rounded-lg">
       <CardHeader className="flex flex-row justify-between">
         <div className="flex-col">
-          <div className="text-2xl font-semibold">Ni Hao</div>
-          <div className="text-xs font-extralight">20 Dec, 2024</div>
+          <div className="text-2xl font-semibold">{props.userId}</div>
+          <div className="text-xs font-extralight">{props.createdAt}</div>
         </div>
         <Emoji />
       </CardHeader>
       <CardContent>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          dignissim, lectus ac volutpat suscipit, nibh dui aliquam justo, eget
-          tempus velit arcu at dolor. Nam ut quam non purus sodales bibendum.
-          Nam molestie iaculis pulvinar. Pellentesque gravida et dolor sed
-          tempor. Vestibulum id fermentum augue, vel vulputate turpis. Aenean
-          consequat auctor arcu, sit amet maximus risus placerat ut. Suspendisse
-          volutpat scelerisque ipsum, vitae elementum velit malesuada eget.
-        </p>
+        <p>{props.content}</p>
       </CardContent>
       <CardFooter>
         <div className="flex gap-2 font-medium leading-none">20 Comments</div>
@@ -38,4 +46,6 @@ export default function ForumCard() {
       </CardFooter>
     </Card>
   );
-}
+};
+
+export default ForumCard;
