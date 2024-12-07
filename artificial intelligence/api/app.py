d88a91ad281690c1f5918cb08f2ccb6f6d6533d2
Admantix <williamwijaya499@gmail.com>
2024-12-07 12:16:50 +0700

    Update Dockerfile and app.py to enhance model dependencies and remove unused endpoints

--- artificial intelligence/api/app.py
+++ artificial intelligence/api/app.py
@@ -3,6 +3,8 @@ import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow as tf
 import re
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from sklearn.preprocessing import LabelEncoder
 from string import punctuation
@@ -67,65 +69,62 @@ def sann_predict():
         
         text = preprocess_text(text)
         text = remove_stopwords(text)
-        max_sequence_length = loaded_model_SANN.input_shape[1]
+        max_sequence_length = 23
         tokenizer = tf.keras.preprocessing.text.Tokenizer()
         tokenizer.fit_on_texts([text]) 
         text_seq = tokenizer.texts_to_sequences([text])
         text_padded = tf.keras.preprocessing.sequence.pad_sequences(text_seq, maxlen=max_sequence_length, padding='post', truncating='post')
-
         prediction = loaded_model_SANN.predict(text_padded)
         prediction_class = prediction.argmax(axis=1)  
-
         encoder = LabelEncoder()
-        
         prediction_label = encoder.inverse_transform(prediction_class)[0]
         
         return jsonify({'prediction': prediction_label}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/mic-predict', methods=['POST'])
-def mic_predict():
-    try:
-        data = request.json
-        if 'input' not in data:
-            return jsonify({'error': 'Invalid Input Data'}), 400
-        text = data['input']
-        if not isinstance(text, str) or len(text.strip()) == 0:
-            return jsonify({'error': 'Invalid Input Data'}), 400
-        max_sequence_length = loaded_model_MIC.input_shape[1]
-        tokenizer = tf.keras.preprocessing.text.Tokenizer()
-        encoder = LabelEncoder()
-        text = preprocess_text(text)
-        text = remove_stopwords(text)
-        text = understand_text(text)
-        text = tokenizer.texts_to_sequences([text])
-        text = tf.keras.preprocessing.sequence.pad_sequences(text, maxlen=max_sequence_length, padding='post', truncating='post')
-        prediction = loaded_model_MIC.predict(text)
-        prediction = prediction.argmax(axis=1)
-        prediction = encoder.inverse_transform(prediction)[0]
-        return jsonify({'prediction': prediction}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+# @app.route('/mic-predict', methods=['POST'])
+# def mic_predict():
+#     try:
+#         data = request.json
+#         if 'input' not in data:
+#             return jsonify({'error': 'Invalid Input Data'}), 400
+#         text = data['input']
+#         if not isinstance(text, str) or len(text.strip()) == 0:
+#             return jsonify({'error': 'Invalid Input Data'}), 400
+#         max_sequence_length = loaded_model_MIC.input_shape[1]
+#         tokenizer = tf.keras.preprocessing.text.Tokenizer()
+#         encoder = LabelEncoder()
+#         text = preprocess_text(text)
+#         text = remove_stopwords(text)
+#         text = understand_text(text)
+#         text = tokenizer.texts_to_sequences([text])
+#         text = tf.keras.preprocessing.sequence.pad_sequences(text, maxlen=max_sequence_length, padding='post', truncating='post')
+#         prediction = loaded_model_MIC.predict(text)
+#         prediction = prediction.argmax(axis=1)
+#         prediction = encoder.inverse_transform(prediction)[0]
+#         return jsonify({'prediction': prediction}), 200
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
 
-@app.route('/salr-predict', methods=['POST'])
-def salr_predict():
-    try:
-        data = request.json
-        if 'input' not in data:
-            return jsonify({'error': 'Invalid Input Data'}), 400
-        text = np.array(data['input'])
-        if text.size == 0:
-            return jsonify({'error': 'Invalid Input Data'}), 400
-        vectorizer = TfidfVectorizer()
-        text = preprocess_text(text)
-        text = remove_stopwords(text)
-        text = understand_text(text)
-        text = vectorizer.transform(text)
-        prediction = loaded_model_SALR.predict(text)
-        return jsonify({'prediction': prediction}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+# @app.route('/salr-predict', methods=['POST'])
+# def salr_predict():
+#     try:
+#         data = request.json
+#         if 'input' not in data:
+#             return jsonify({'error': 'Invalid Input Data'}), 400
+#         text = np.array(data['input'])
+#         if text.size == 0:
+#             return jsonify({'error': 'Invalid Input Data'}), 400
+#         vectorizer = TfidfVectorizer()
+#         text = preprocess_text(text)
+#         text = remove_stopwords(text)
+#         text = understand_text(text)
+#         text = vectorizer.transform(text)
+#         prediction = loaded_model_SALR.predict(text)
+#         return jsonify({'prediction': prediction}), 200
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
