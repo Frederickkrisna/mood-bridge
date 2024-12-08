e0272a5e07c9776b70a9a648cbc466e2fc44dd2d
Admantix <williamwijaya499@gmail.com>
2024-12-08 18:43:45 +0700

    Fetch and display post details alongside comments in the comment section

--- front-end/src/app/dashboard/comment.tsx
+++ front-end/src/app/dashboard/comment.tsx
@@ -2,14 +2,31 @@ import RightSidebar from "@/components/right-sidebar";
 import Layout from "../layout";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { CommentInterface } from "@/interfaces/interface";
+import { CommentInterface, PostInterface } from "@/interfaces/interface";
 import CommentCard from "@/components/comment-card";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "@/lib/firebase";
+import ForumCard from "@/components/forum-card";
 
 export default function Comment() {
   const { id } = useParams<string>();
   const [comments, setComments] = useState<CommentInterface[]>([]);
+  const [post, setPost] = useState<PostInterface>({
+    id: "",
+    content: "",
+    mood: "",
+    createdAt: "",
+    userId: "",
+    first_name: "",
+    last_name: "",
+  });
 
   useEffect(() => {
     const fetchComments = async (postId: string) => {
@@ -28,8 +45,36 @@ export default function Comment() {
         return;
       }
     };
+
+    const getPost = async (postId: string) => {
+      try {
+        const docRef = doc(db, "MsPost", postId);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          const createdAt = data.createdAt.toDate().toLocaleString();
+          return {
+            id: docSnap.id,
+            ...data,
+            createdAt,
+          };
+        } else {
+          return null;
+        }
+      } catch (e) {
+        console.error("Error getting document:", e);
+        return null;
+      }
+    };
+
     if (id) {
       fetchComments(id);
+      const docPost = getPost(id);
+      docPost.then((data) => {
+        if (data) {
+          setPost(data as PostInterface);
+        }
+      });
     }
   }, [id]);
 
@@ -37,6 +82,7 @@ export default function Comment() {
     <Layout>
       <div className="flex flex-row min-h-screen">
         <div className="flex-1 flex flex-col w-[50vw] overflow-x-hidden px-4 ml-[45vh]">
+          {post && <ForumCard {...post} />}
           <div className="flex flex-col mt-4">
             {comments.map((comment) => (
               <CommentCard key={comment.id} {...comment} />
